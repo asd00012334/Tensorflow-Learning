40e88a4801bcfb06d559daaa1c81f7c2f9bc3f52
Taylor Huang <asd00012334@hotmail.com>
2017-02-01 11:02:07 +0800

    Update ResultVisualizationPractice.py

--- Basics/ResultVisualizationPractice.py
+++ Basics/ResultVisualizationPractice.py
@@ -1,3 +1,39 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def addLayer(inputs, inSize, outSize, act=None):
+	weights=tf.Variable(tf.random_normal([inSize,outSize]))
+	biases=tf.Variable(tf.zeros([1,outSize])+0.1)
+	linearOut=tf.matmul(inputs,weights)+biases
+	if(act==None):
+		return linearOut
+	else:
+		return act(linearOut)
+
+# Create Test Case Input
+xData=np.linspace(-1,1,300)[:,np.newaxis]
+noise=np.random.normal(0,0.05,xData.shape)
+yData=np.square(xData)-0.5
+
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+ax.scatter(xData,yData)
+plt.show(block=False)
+
+# Structure
+xInput=tf.placeholder(tf.float32,[None,1])
+yInput=tf.placeholder(tf.float32,[None,1])
+
+# Add a hidden layer and output layer
+layer1=addLayer(xInput,1,10,act=tf.nn.relu)
+prediction=addLayer(layer1,10,1,act=None)
+
+Error=tf.reduce_mean(
+	tf.reduce_sum(
+		tf.square(yInput-prediction),
+		reduction_indices=[1]
+	)
 )
 
 train=tf.train.GradientDescentOptimizer(0.1).minimize(Error)
